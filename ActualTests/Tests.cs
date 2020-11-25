d57a0fa7a590d6761988246725611cd28ec388c4
vlad_veko <veko.2001@gmail.com>
2020-11-25 23:12:57 +0300

    Initial commit

---
+++ ActualTests/Tests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ActualTests
+{
+    [TestFixture]
+    public class MathTests
+    {
+        [Test]
+        public void Add_Add4and3_ResultIs7()
+        {
+
+        }
+
+        [Test]
+        public void Max_MaxOf5and5_ResultIs5()
+        {
+
+        }
+    }
+
+    [TestFixture]
+    public class OrderServiceTests
+    {
+
+    }
+    
+    [TestFixture]
+    public class ProductTests
+    {
+
+    }
+
+    [TestFixture]
+    public class Reservation
+    {
+        [Test]
+        public void GetPrice_CustomerIsGoldFalse_Price()
+        {
+
+        }
+
+        [Test]
+        public void GetPrice_CustomerIsGoldTrue_PriceIsLower()
+        {
+
+        }
+    }
+}
---
+++ UnitTests/Math.cs
@@ -0,0 +1,15 @@
+﻿namespace UnitTests
+{
+    public static class Math
+    {
+        public static int Add(int a, int b)
+        { 
+            return a + b;
+        }
+
+        public static int Max(int a, int b)
+        {
+            return (a > b) ? a : b;
+        }
+    }
+}
